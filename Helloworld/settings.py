f8e390b784841cc658a8b09c53682596733d50bd
BraveAction <418405228@qq.com>
2017-08-11 00:28:56 +0800

    fix:修改django项目访问地址的问题

--- Helloworld/settings.py
+++ Helloworld/settings.py
@@ -24,7 +24,7 @@ SECRET_KEY = 'ggtc1%jdp9_+cwr&^$gj6w_@2rav)xr8dh$l4=#mva1uypo-+)'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 # Application definition
 
